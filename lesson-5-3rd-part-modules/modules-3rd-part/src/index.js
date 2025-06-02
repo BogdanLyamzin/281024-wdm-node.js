32e7ae70b4eac32b2dd1238e26e2eec82edfa296
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-06-02 10:14:55 +0200

    add luxon example

---
+++ lesson-5-3rd-part-modules/modules-3rd-part/src/index.js
@@ -0,0 +1,51 @@
+// const date = new Date("2025-05-31");
+
+// const dateInStr = "30/04/2025";
+// const [day, month, year] = dateInStr.split("/");
+// const date2 = new Date(`${year}-${month}-${day}`);
+
+// console.log(date);
+// console.log(date2);
+
+// console.log(date);
+// console.log(date.getHours());
+// console.log(date.toISOString());
+
+// const date = new Date();
+// console.log(JSON.stringify(date));
+// const date = new Date();
+// console.log(date);
+// date.setHours(date.getHours() + 5);
+// console.log(date);
+
+// const date = new Date();
+// console.log(date.toLocaleDateString())
+// const year = date.getFullYear();
+// const month = date.getMonth() + 1;
+// const day = date.getDate();
+// const formatDate = `${year}/${month}/${day}`;
+// console.log(formatDate);
+
+import { DateTime } from "luxon";
+
+// const now = DateTime.now();
+// console.log(now);
+// const now2 = DateTime.local();
+// console.log(now2);
+// const nowUTC = DateTime.utc();
+// console.log(nowUTC);
+// console.log(now.year);
+// console.log(now.month);
+// console.log(now.day);
+// const {year, month, day} = now;
+// const nowLA = DateTime.now().setZone("America/Los_Angeles");
+// console.log(nowLA);
+// const now = DateTime.now();
+// const now2 = now.plus({days: 2, hours: 1});
+// console.log(now);
+// console.log(now2);
+// console.log(now.toFormat("dd/MM/yyyy"));
+// console.log(now.toISO())
+// const ISOdate = "2025-06-02T10:09:43.217+02:00";
+// const date = new DateTime(ISOdate);
+// console.log(date);
