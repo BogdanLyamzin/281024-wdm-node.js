fae3e74205978d7ac997cd599f47e78197c21e10
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-06-12 10:28:25 +0200

    add connect to database

--- lesson-10-sequelize-cli/animal-eshop-backend/src/index.js
+++ lesson-10-sequelize-cli/animal-eshop-backend/src/index.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 
+import { connectDatabase } from "./db/sequelize.js";
 import startServer from "./server.js";
 
 const bootstrap = async()=> {
+    await connectDatabase(); // throw new Error()
     startServer();
 }
 
-bootstrap();
+bootstrap(); // throw new Error()
