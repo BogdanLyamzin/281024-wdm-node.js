a1fede622e6b14c35716cbd33849a62b0ff2fae4
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-06-30 12:11:01 +0200

    fix

---
+++ lesson-21-enter-typescript/typescript-nodejs-project/src/classes/User.ts
@@ -0,0 +1,9 @@
+export default class User {
+    name: string;
+    lastName: string;
+    
+    constructor(name: string, lastName: string) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
--- lesson-21-enter-typescript/typescript-nodejs-project/src/index.ts
+++ lesson-21-enter-typescript/typescript-nodejs-project/src/index.ts
@@ -1,20 +1,74 @@
-const calcWeightIndex = (weight: number, height: number) => {
-  const result = weight / height ** 2;
-  return Number(result.toFixed(2));
-};
+const userName: string = "Bohdan";
+const userAge: number = 39;
+const userRest: boolean = false;
+const userPK: null = null;
 
-console.log(calcWeightIndex(90, 1.9));
+const getFullName = (name: string, lastName: string): string => {
+    return `${name} ${lastName}`;
+}
 
-class User {
-    name: string;
+const fullName = getFullName("Bohdan", "Liamzin");
+//@ ts-expect-error
+// console.log(fullName.toFixed(2));
+const logMessage = (message: string): void => {
+    console.log(message);
+}
+// logMessage("Welcome");
+
+const findShortestName = (...arr: string[])=>{ // Array<string>
+    return arr.reduce((acum: string, str: string)=> {
+        if(str.length > acum.length) return str;
+        return acum;
+    });
+}
+
+// findShortestName("Bohdan", "Alina", "Anna");
+
+type Currency = "usd" | "euro";
+
+const calcSallary = (value: number, currency: Currency)=> {
+ switch(currency) {
+    case "usd":
+        return value / 42;
+    case "euro":
+        return value / 48;
+ }
+}
+
+console.log(calcSallary(10000, "usd"));
+console.log(calcSallary(10000, "euro"));
+console.log(calcSallary(10000, "funt"));
+
+type User = {
+    name: string; 
     lastName: string;
-    
-    constructor(name: string, lastName: string) {
-        this.name = name;
-        this.lastName = lastName;
-    }
+    fullName: string;
+}
 
-    async fetchAll(){
-      
+const createUser = (name: string, lastName: string): User => {
+    return {
+        name,
+        lastName,
+        get fullName(){
+            return `${this.name} ${this.lastName}`
+        }
     }
 }
+
+const newUser = createUser("Bohdan", "Liamzin");
+
+interface Book {
+    name: string;
+    description?: string;
+    year: number;
+}
+
+const createBook = (name: string, year: number, description: string | undefined): Book => {
+    return {
+        name,
+        year,
+        description,
+    }
+}
+
+const newBook = createBook("Преследование Аделин", 2009);
