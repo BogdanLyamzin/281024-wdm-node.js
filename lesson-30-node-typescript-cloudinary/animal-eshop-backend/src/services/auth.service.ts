7324cb5f41efa2eb75974882218d2d16f3f2cad5
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-07-15 12:00:32 +0200

    lesson 30

--- lesson-30-node-typescript-cloudinary/animal-eshop-backend/src/services/auth.service.ts
+++ lesson-30-node-typescript-cloudinary/animal-eshop-backend/src/services/auth.service.ts
@@ -20,14 +20,18 @@ interface IUserFind {
   email: string;
 }
 
+interface IJWTTokenPayload {
+  id: unknown;
+}
+
 const { JWT_SECRET } = process.env;
 
-const createToken = (user: UserDocument) => {
-  const payload = {
+const createToken = (user: UserDocument): string => {
+  const payload: IJWTTokenPayload = {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, JWT_SECRET as string, { expiresIn: "24h" });
+  const token: string = jwt.sign(payload, JWT_SECRET as string, { expiresIn: "24h" });
   return token;
 }
 
@@ -35,15 +39,15 @@ export const login = async ({ email, password }: Login): Promise<ILoginResponse>
   const userFind: IUserFind = {
     email,
   };
-  
+
   const user: UserDocument | null = await User.findOne(userFind);
 
   if (!user) throw HttpExeption(401, `User with email=${email} not found`); // throw HttpExeption(401, "Email or password invalid");
 
-  const passwordCompare = await bcrypt.compare(password, user.password);
+  const passwordCompare: boolean = await bcrypt.compare(password, user.password);
   if (!passwordCompare) throw HttpExeption(401, `Password invalid`);
 
-  const token = createToken(user);
+  const token: string = createToken(user);
   user.token = token;
   await user.save();
 
@@ -56,8 +60,8 @@ export const login = async ({ email, password }: Login): Promise<ILoginResponse>
   };
 };
 
-export const getCurrent = async user => {
-  const token = createToken(user);
+export const getCurrent = async (user: UserDocument): Promise<ILoginResponse> => {
+  const token: string = createToken(user);
   user.token = token;
   await user.save();
 
@@ -70,8 +74,8 @@ export const getCurrent = async user => {
   };
 }
 
-export const logout = async ({_id})=> {
-  const user = await User.findById(_id);
+export const logout = async ({_id}: UserDocument): Promise <void> => {
+  const user: UserDocument | null = await User.findById(_id);
   if (!user) throw HttpExeption(401, `User not found`);
   user.token = "";
   await user.save();
