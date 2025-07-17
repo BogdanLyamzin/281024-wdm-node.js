f716ddbcb5d5ba0028f50175b4ea2262c555bbe4
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-07-17 09:58:25 +0200

    lesson 32

---
+++ lesson-32-node-typescript-cloudinary/animal-eshop-backend/src/db/User.ts
@@ -0,0 +1,49 @@
+import {Schema, model, Document} from "mongoose";
+
+import { emailValidation } from "../constants/users.constants";
+
+import { Role } from "../typescript/types";
+
+interface User {
+  fullName: string;
+  email: string;
+  password: string;
+  role: Role,
+  token?: string;
+}
+
+export type UserDocument = User & Document;
+
+const userSchema = new Schema<UserDocument>({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: emailValidation.value,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: {
+      values: ["superadmin", "admin", "manager", "user"],
+      message: "user/manager/admin with with email already exist",
+    },
+    default: "user",
+    required: true,
+  },
+  token: {
+    type: String,
+  }
+}, {versionKey: false, timestamps: true});
+
+const User = model<UserDocument>("user", userSchema);
+
+export default User;
+
