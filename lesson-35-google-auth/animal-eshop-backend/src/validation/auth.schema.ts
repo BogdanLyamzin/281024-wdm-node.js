9e4d313da13898b13522afff016a3adf374da7c5
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-07-30 14:17:22 +0200

    finish lesson 35

---
+++ lesson-35-google-auth/animal-eshop-backend/src/validation/auth.schema.ts
@@ -0,0 +1,27 @@
+import * as Yup from "yup";
+
+import { passwordSchema, emailSchema } from "./users.schema";
+
+export const confirmGoogleOauthSchema = Yup.object({
+    code: Yup.string().trim().required(),
+})
+
+export const registerSchema = Yup.object({
+    fullName: Yup.string().trim().required(),
+    email: emailSchema,
+    password: passwordSchema,
+})
+
+export const verifyCodeSchema = Yup.object({
+    code: Yup.string().trim().required(),
+})
+
+export const loginSchema = Yup.object({
+    email: emailSchema,
+    password: passwordSchema,
+});
+
+export type GoogleOauth = Yup.InferType<typeof confirmGoogleOauthSchema>;
+export type Register = Yup.InferType<typeof registerSchema>;
+export type VerifyCode = Yup.InferType<typeof verifyCodeSchema>;
+export type Login = Yup.InferType<typeof loginSchema>;
